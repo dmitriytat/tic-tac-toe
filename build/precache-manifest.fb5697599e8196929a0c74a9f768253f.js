8f7c106dac6ae41654b16f402be2c1ef6b215a83
Dmitriy Tatarintsev <tatarintsevd@gmail.com>
2019-01-31 02:19:52 +0300

    add builded

---
+++ build/precache-manifest.fb5697599e8196929a0c74a9f768253f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "584e9b829d45556aa539",
+    "url": "/static/js/main.584e9b82.chunk.js"
+  },
+  {
+    "revision": "9e937cc80e0766221e63",
+    "url": "/static/js/1.9e937cc8.chunk.js"
+  },
+  {
+    "revision": "584e9b829d45556aa539",
+    "url": "/static/css/main.86d49f5f.chunk.css"
+  },
+  {
+    "revision": "524a61f7e7572ae329020fcbb5f96bd2",
+    "url": "/index.html"
+  }
+];
